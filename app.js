c17766e014ca38622a845d199d2ed66ebfab6fef
Jørgen Stamnes <jorgenstamnes@Jrgens-MacBook-Pro.local>
2019-10-03 13:15:28 +0200

    Ny branch

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => res.send("ASDASDSDDASD"));
+app.use("/", (req, res) => res.send("Ny branch for helvette!"));
 
 app.listen(8000, () => console.log("listening on 8000"));
