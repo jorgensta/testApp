3477cf1ca3faeeab4da8d7745eb04f7ed602f20c
Jørgen Stamnes <jorgenstamnes@Jrgens-MacBook-Pro.local>
2019-10-03 13:20:28 +0200

    Add gitignore

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => res.send("Ny branch for helvette!"));
+app.use("/", (req, res) => res.send("Ny branch enda noe nytt!"));
 
 app.listen(8000, () => console.log("listening on 8000"));
